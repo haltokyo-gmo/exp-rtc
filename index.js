351bce911b43c8fc6cbf6d63004b98cb20e215e0
himu62 <zero.256dic@gmail.com>
2016-10-12 16:25:08 +0900

    add emotion

--- index.js
+++ index.js
@@ -1,4 +1,38 @@
-(function() {
+$(function() {
+	function MaxEmotion(scores) {
+		var max = Math.max(
+			scores.anger,
+			scores.contempt,
+			scores.disgust,
+			scores.fear,
+			scores.happiness,
+			scores.neutral,
+			scores.sadness,
+			scores.surprise
+		);
+
+		switch(max) {
+			case scores.anger:
+				return 'anger ' + scores.anger.toFixed(4);
+			case scores.contempt:
+				return 'contempt ' + scores.contempt.toFixed(4);
+			case scores.disgust:
+				return 'disgust ' + scores.disgust.toFixed(4);
+			case scores.fear:
+				return 'fear ' + scores.fear.toFixed(4);
+			case scores.happiness:
+				return 'happiness ' + scores.happiness.toFixed(4);
+			case scores.neutral:
+				return 'neutral ' + scores.neutral.toFixed(4);
+			case scores.sadness:
+				return 'sadness ' + scores.sadness.toFixed(4);
+			case scores.surprise:
+				return 'surprise ' + scores.surprise.toFixed(4);
+			default:
+				return 'none';
+		}
+	}
+
 	navigator.mediaDevices.getUserMedia({video: {
 		mandatory: {
 			maxWidth: 600,
@@ -18,55 +52,48 @@
 	var ctx = canvas.getContext('2d');
 	ctx.scale(-1, 1);
 
-	function capture() {
-		ctx.drawImage(document.querySelector('#video'), -600, 0);
-	}
-
-	var countdown = 0;
-	function count() {
-		document.querySelector('#countdown').textContent = countdown;
-		countdown--;
-	}
-
 	document.querySelector('#button').addEventListener('click', function() {
-		setTimeout(capture, 10000);
-		countdown = 10;
-		count();
-		setInterval(count, 1000);
+		ctx.drawImage(document.querySelector('#video'), -600, 0);
+		var base64 = canvas.toDataURL('image/jpeg');
+		var bin = atob(base64.replace(/^.*,/, ''));
+		var buffer = new Uint8Array(bin.length);
+		for (var i = 0; i < bin.length; i++) {
+	        buffer[i] = bin.charCodeAt(i);
+	    }
 
-		// ctx.drawImage(document.querySelector('#video'), -600, 0);
-		// canvas.toBlob(function(blob) {
-		// 	$.ajax({
-		// 		cache: false,
-		// 		contentType: 'application/json',
-		// 		data: {
-		// 			"url": URL.createObjectURL(blob)
-		// 		},
-		// 		dataType: 'json',
-		// 		headers: {
-		// 			'Ocp-Apim-Subscription-Key': 'subscription key here'
-		// 		},
-		// 		method: 'POST',
-		// 		processData: false,
-		// 		url: 'https://api.projectoxford.ai/emotion/v1.0/recognize'
-		// 	})
-		// 	.done(function(data) {
-		// 		for(var i in data) {
-		// 			var rect = data[i].faceRectangle;
-		// 			var text = MaxEmotion(data[i].scores);
-		//
-		// 			ctx.beginPath();
-		// 			ctx.strokeRect(rect.left, rect.top, rect.width, rect.height);
-		// 			ctx.fillStyle = 'black';
-		// 			var m = ctx.measureText(text);
-		// 			ctx.fillRect(rect.left, rect.top - 10, m.width, 10);
-		// 			ctx.fillStyle = 'white';
-		// 			ctx.fillText(text, rect.left, rect.top);
-		// 		}
-		// 	})
-		// 	.fail(function(error) {
-		// 		console.log(error);
-		// 	})
-		// }, 'image/jpeg', 0.95);
+		$.ajax({
+			cache: false,
+			contentType: 'application/octet-stream',
+			data: buffer.buffer,
+			dataType: 'json',
+			headers: {
+				'Ocp-Apim-Subscription-Key': 'subscription key here'
+			},
+			method: 'POST',
+			processData: false,
+			url: 'https://api.projectoxford.ai/emotion/v1.0/recognize'
+		})
+		.done(function(data) {
+			var canvas_overlay = document.querySelector('#canvas-overlay');
+			var ctx_overlay = canvas_overlay.getContext('2d');
+			ctx_overlay.clearRect(0, 0, canvas_overlay.width, canvas_overlay.height);
+
+			for(var i in data) {
+				var rect = data[i].faceRectangle;
+				var text = MaxEmotion(data[i].scores);
+
+				ctx_overlay.beginPath();
+				ctx_overlay.strokeRect(rect.left, rect.top, rect.width, rect.height);
+				ctx_overlay.fillStyle = 'black';
+				var m = ctx_overlay.measureText(text);
+				ctx_overlay.fillRect(rect.left, rect.top - 10, m.width, 10);
+				ctx_overlay.fillStyle = 'white';
+				ctx_overlay.fillText(text, rect.left, rect.top);
+				ctx_overlay.closePath();
+			}
+		})
+		.fail(function(error) {
+			console.log(error);
+		})
 	})
-})();
+})
